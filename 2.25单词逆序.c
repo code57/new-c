3b0af83bb51bc2fdb421e1cbe657d30f950fbe8a
code57 <124147576+code57@users.noreply.github.com>
2023-02-26 21:59:43 +0800

    2.25

--- 2.25单词逆序.c
+++ 2.25单词逆序.c
@@ -33,3 +33,41 @@ int main()
 	prit(&arr);
 	return 0;
 }
+//将一句话的单词进行倒置，标点不倒置。
+//bit做法
+void reverse(char* left, char* right)
+{
+	while (left < right)
+	{
+		char tmp = 0;
+		tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+
+int main()
+{
+	char arr[100] = { 0 };
+	gets(arr);
+	int len = strlen(arr);
+	reverse(arr, arr + len - 1);
+	char* start = arr;
+	while (*start)
+	{
+		char* end = start;
+		while (*end != ' ' && *end != '\0')
+		{
+			end++;
+		}
+		reverse(start, end - 1);
+		if (*end == ' ')
+			start = end + 1;
+		else
+			start = end;
+	}
+	printf("%s\n", arr);
+	return 0;
+}
