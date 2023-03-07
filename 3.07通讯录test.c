6157911da4219d08bbfc5ae5f95474db684d1788
code57 <124147576+code57@users.noreply.github.com>
2023-03-07 21:13:20 +0800

    3.07

---
+++ 3.07通讯录test.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+
+
+
+
+void menu()
+{
+	printf("***************通讯录*****************\n");
+	printf("********1.add      2.del   ***********\n");
+	printf("********3.search   4.modify***********\n");
+	printf("********5.sort     6.print ***********\n");
+	printf("********      0.exit       ***********\n");
+	printf("**************************************\n");
+	printf("\n");
+}
+
+enum option
+{
+	e_xit,
+	add,
+	del,
+	search,
+	modify,
+	sort,
+	print
+};
+
+int main()
+{
+	int input = 0;
+	contact con;
+	initcontact(&con);
+	do
+	{
+		menu();
+		printf("请选择\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case e_xit:
+			free(con.data);
+			con.data = NULL;//动态版本内存销毁
+			con.sz = 0;
+			con.capa = 0;
+			return 0;
+			break;
+		case add:
+			addcontact(&con);
+			break;
+		case del:
+			delcontact(&con);
+			break;
+		case search:
+			searchcontact(&con);
+			break;
+		case modify:
+			modifycontact(&con);
+			break;
+		case sort:
+			sortcontact(&con);
+			break;
+		case print:
+			printcontact(&con);
+			break;
+		default:
+			printf("输入错误，请重新输入！\n");
+			break;
+		}
+	} while (input);
+
+
+
+
+
+	return 0;
+}
