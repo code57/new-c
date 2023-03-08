f16c359fb934a0dcb70352b6332e99836e4141c7
code57 <124147576+code57@users.noreply.github.com>
2023-03-08 21:15:24 +0800

    3.08

---
+++ 3.08文件写入读取.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()//文件写入与输出
+{
+	FILE* p = fopen("test.dat", "w");
+	if (p == NULL)
+	{
+		perror("fopen");
+		return 1;
+	}
+	fputs("lmg", p);
+	fputc('b', p);
+	fputc('c', p);
+	fputc('d', p);
+	fclose(p);
+
+	p = fopen("test.dat", "r+");
+	if (p == NULL)
+	{
+		perror("fopen");
+		return 1;
+	}
+	int ret = fgetc(p);
+	printf("%c\n", ret);
+	ret = fgetc(p);
+	printf("%c\n", ret);
+	ret = fgetc(p);
+	printf("%c\n", ret);
+	ret = fgetc(p);
+	printf("%c\n", ret);
+
+	fclose(p);
+	p = NULL;
+	return 0;
+}
